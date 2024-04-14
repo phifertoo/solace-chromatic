a30ba8c0e72dab5cf4a3c611e76289212b20f598
Lance Watanabe <phiferthree@gmail.com>
2024-04-14 15:34:54 -0700

    bbb

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -5,7 +5,7 @@ const EmptyState = () => {
   return (
     <Center flexDirection="column" mt="20">
       <Text fontSize="lg" mb="4">
-        aaa. You have no notes yet. Create your first note below!
+        bbb. You have no notes yet. Create your first note below!
       </Text>
     </Center>
   );
